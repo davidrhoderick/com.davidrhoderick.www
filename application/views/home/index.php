8efaaf3805d519a5585c71a301b6f83402abba8a
David Rhoderick <broken.stairs@gmail.com>
2012-09-16 19:52:45 -0400

    Trying to fix home display

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,14 +1,12 @@
 <!-- <?= $this->load->view('home/promo'); ?> -->
 
-<section class="main">
-	<div class="frame cfx">
+<section>
+	<div>
 
-		<section class="left_column">
-			<?= $this->load->view('modules/about'); ?> <!-- Why isn't this displaying -->
-			<?= $this->load->view('modules/pieces'); ?>
-		</section>
-		<section class="sidebar">
-			<?= $this->load->view('modules/mediaplayer'); ?>
+		<section>
+			<?= $this->load->view('modules/about'); ?>
+			<!-- <?= $this->load->view('modules/pieces'); ?>
+			<?= $this->load->view('modules/mediaplayer'); ?> -->
 		</section>
 	</div>
 </section><!-- end main section -->
