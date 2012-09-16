49e07fbd762e7cca8ff718cf9ffaba17539faa7c
David Rhoderick <broken.stairs@gmail.com>
2012-09-16 19:16:35 -0400

    Eliminating commented out social links from oap

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,10 +12,6 @@
 				<a href="/">Home/About</a>
 				<a href="/books">Pieces</a>
 				<a href="/about">Contact</a>
-				<!-- <a class="social_icon" target="_blank" href="http://www.facebook.com/pages/Open-Air-Publishing/156086657797116" title="Friend Open Air Publishing on Facebook"><img src="/images/icon_facebook_24.gif" alt="Friend Open Air Publishing on Facebook" /></a>
-				<a class="social_icon" target="_blank" href="http://www.twitter.com/openairbooks" title="Follow Open Air Publishing on Twitter"><img src="/images/icon_twitter_24.gif" alt="Follow Open Air Publishing on Twitter" /></a>
-				<a class="social_icon" target="_blank" href="http://pinterest.com/openairpub/" title="Follow Open Air Publishing on Pinterest"><img src="/images/icon_pinterest_24.gif" alt="Follow Open Air Publishing on Pinterest" /></a>
-				<a class="social_icon" href="/subscribe" title="Subscribe to the Open Air Publishing Newsletter"><img src="/images/icon_email_24.gif" alt="Subscribe to the Open Air Publishing Newsletter" /></a> -->
 			</nav>
 		</div>
 	</header>
