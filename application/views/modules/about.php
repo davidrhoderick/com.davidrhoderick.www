721e85addc364492fd857ceef757bbaf674222e3
David Rhoderick <broken.stairs@gmail.com>
2012-09-16 20:15:54 -0400

    trying to fix it not working all of a sudden

--- application/views/modules/about.php
+++ application/views/modules/about.php
@@ -1,2 +1,2 @@
 	<h2>About David Rhoderick</h2>
-	<p>David is <?php echo date( 'Y-m-d' ) - (date)1988-08-26; ?> years old.</p>
+	<p>David is <?php echo date( 'Y-m-d' ) - 1988-08-26; ?> years old.</p>
