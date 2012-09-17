b3544b2362e8aeced4d95284aed2c91ffa13ead3
David Rhoderick <broken.stairs@gmail.com>
2012-09-16 20:06:51 -0400

    Trying to fix commenting errors

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,9 +3,6 @@
 
 		<section>
 			<?= $this->load->view('modules/about'); ?>
-			<?= //$this->load->view('modules/pieces'); ?>
-			//<?= $this->load->view('modules/mediaplayer');
-			?>
 		</section>
 	</div>
 </section><!-- end main section -->
