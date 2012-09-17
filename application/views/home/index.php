82eaea40d6c69baa49edebb0d7266bb5c25fb91b
David Rhoderick <broken.stairs@gmail.com>
2012-09-16 20:06:10 -0400

    Fixing website display

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,8 +3,9 @@
 
 		<section>
 			<?= $this->load->view('modules/about'); ?>
-			<!-- <?= $this->load->view('modules/pieces'); ?>
-			<?= $this->load->view('modules/mediaplayer'); ?> -->
+			<?= //$this->load->view('modules/pieces'); ?>
+			//<?= $this->load->view('modules/mediaplayer');
+			?>
 		</section>
 	</div>
 </section><!-- end main section -->
