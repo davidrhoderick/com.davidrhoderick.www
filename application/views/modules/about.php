90c36873e67e22439dc7cd697243cb3b6e4e9b34
David Rhoderick <broken.stairs@gmail.com>
2012-09-16 20:49:33 -0400

    updating about module

--- application/views/modules/about.php
+++ application/views/modules/about.php
@@ -1,2 +1,2 @@
 	<h2>About David Rhoderick</h2>
-	<p>David is <?php echo date( 'Y-m-d' ) - "1988-08-26"; ?> years old.</p>
+	<p>David is <?php echo date( 'Y-m-d' ) - "1988-08-26"; ?> years old. He is currently a graduate student studying at the School of Visual Arts in New York for a Computer Art MFA degree.  He started as a self-produced musician and eventually became a broadcast video producer and camera operator.  Today, his work is characterized by use of code to experiment with both automation and data manipulation in pieces that attempt to take a pragmatic approach to our relationship to our environments as facilitated through our consciousness.</p>
