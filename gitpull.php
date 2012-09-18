042b20a5208a229bf2742d768935ba8f2e896e7d
David Rhoderick <broken.stairs@gmail.com>
2012-09-18 12:44:27 -0400

    commenting out shell_exec

--- gitpull.php
+++ gitpull.php
@@ -3,7 +3,7 @@
 		
 	$payload = json_decode(stripslashes(@$_POST['payload']));
 	$message = print_r(@$payload,true);
-	$message .= shell_exec('/usr/bin/git pull');
+	//$message .= shell_exec('/usr/bin/git pull');
 	mail('broken.stairs@gmail.com','gihub post receive hook fired',$message);
 	echo $message;
 	exit;
