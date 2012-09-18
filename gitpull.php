8bf44df9d25586acb80d7a40f258a0186c7c5b93
David Rhoderick <broken.stairs@gmail.com>
2012-09-18 10:23:55 -0400

    Fixing gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,11 +1,9 @@
 <?php 
 	error_reporting(0);
-	
-	echo "here it is!";
 		
 	$payload = json_decode(stripslashes(@$_POST['payload']));
 	$message = print_r(@$payload,true);
-	//$message .= shell_exec('/usr/bin/git pull');
+	$message .= shell_exec('/usr/bin/git pull');
 	mail('broken.stairs@gmail.com','gihub post receive hook fired',$message);
 	echo $message;
 	exit;
