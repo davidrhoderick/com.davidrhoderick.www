1f79fe62155eee738fceac78c4b9c0ff1d576b60
David Rhoderick <broken.stairs@gmail.com>
2012-09-17 22:20:57 -0400

    copying and adapting from someone else

--- gitpull.php
+++ gitpull.php
@@ -1,4 +1,27 @@
-<?php
-	$output = `mkdir itworked`;
-	echo "<pre$output</pre>";
+<?php 
+	// Turn off error reporting
+	error_reporting(0);
+
+	try
+	{
+			// Decode the payload json string
+			$payload = json_decode($_REQUEST['payload']);
+	}
+	catch(Exception $e)
+	{
+			exit;
+	}
+
+	// Pushed to master?
+	if ($payload->ref === 'refs/heads/master')
+	{
+			// Log the payload object
+			@file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+			// Prep the URL - replace https protocol with git protocol to prevent 'update-server-info' errors
+			$url = str_replace('https://', 'git://', $payload->repository->url);
+
+			// Run the build script as a background process
+			`git pull`;
+	}
 ?>
