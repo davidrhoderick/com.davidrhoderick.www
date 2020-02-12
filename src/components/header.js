afe7c9134b7613f499e95f3d8659cd8a64942e7a
David Rhoderick <david@artlyticalmedia.com>
2020-02-12 21:33:49 +0100

    Adding all files for gatsby version

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to="/about">about</Link></li>
+        <li><Link to="/">LOGO</Link></li>
+        <li><Link to="/portfolio">portfolio</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
